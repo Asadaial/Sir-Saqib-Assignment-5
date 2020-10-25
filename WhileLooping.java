1e1fb4887cf0510b8cfa1a28f7985c22a1712f31
asaddeveloper <aiakhokhar@gmail.com>
2020-10-25 23:27:03 +0500

    1st addition

---
+++ WhileLooping.java
@@ -0,0 +1,14 @@
+public class WhileLooping {  
+   public static void main(String[] args) {
+   	int n=0;
+   	int i=0;
+
+   	while (i<5) {
+
+  			System.out.println(i);
+  			n++;
+  			i++;
+			}
+		System.out.println("The loop run for "+n+" times");
+   }
+ }  
---
+++ dec.java
@@ -0,0 +1,12 @@
+public class dec {  // Save as "OddEvenSum.java"
+   public static void main(String[] args) {
+      int a=0;
+      int b=5;
+      int c=10;
+      int sum=a+b+c;
+      System.out.println("the value store in a= "+a);
+      System.out.println("the value store in b= "+b);
+      System.out.println("the value store in c= "+c);
+      System.out.println("the value store in sum= "+sum);
+   }
+}
